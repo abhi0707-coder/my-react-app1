169c78f4a28d11a9a545be4b9ff0c916f8180fcc
Abhijeet Pardeshi <abhijeet.pardeshi1997gmail@.com>
2021-07-29 16:26:36 +0530

    demo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
     <div>
-    <h1>Hello Abhijeet</h1>
-      <h2>I am abhijeet pardeshi</h2>
-      <h3>hello</h3>
-   </div>
-  );
+      {' '}
+      <h1>hello </h1>
+      <h2>abhijeet pardeshi Rajput</h2>
+    </div>
+  )
 }
 
-export default App;
+export default App
