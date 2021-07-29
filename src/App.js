3cee7c3743dfed11bd7351eac85b9e71c08b8703
Abhijeet Pardeshi <abhijeet.pardeshi1997gmail@.com>
2021-07-29 22:23:33 +0530

    map.list

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
+  let title = 'hello India'
+  let list = ['delhi', 'mumbai', 'calcutta', 'chenai', 'pune', 'lonaval']
   return (
     <div>
-      {' '}
-      <h1>hello </h1>
-      <h2>abhijeet pardeshi Rajput</h2>
+      <h1>{title}</h1>
+      <ul>
+        {list.map((item) => {
+          return <li> {item}</li>
+        })}
+      </ul>
     </div>
   )
 }
