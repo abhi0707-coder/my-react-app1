c29c3c208cc59d7cdbcb2070002e4448fb1171ad
Abhijeet Pardeshi <abhijeet.pardeshi1997gmail@.com>
2021-07-29 23:02:09 +0530

    onClick

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  let list = ['Hiii', 'Hiii']
+  let list = ['Hello', 'Hello']
+
+  let addNewHello = () => {
+    //alert('Add new hello logic will come here')
+    list.push('hello')
+    console.log(list)
+  }
 
   return (
     <div>
-      <h1>Working event </h1>
-      <input type="button" value="say hii" />
-
-      {list.map((item) => {
-        return <h3>{item}</h3>
-      })}
+      <h1>Working with event and list</h1>
+      <input type="button" value="say hello" onClick={addNewHello} />
+      {list.map((item) => (
+        <h3>{item}</h3>
+      ))}
     </div>
   )
 }
