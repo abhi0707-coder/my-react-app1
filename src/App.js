8f2862940971e2dc1eb19b94055f477f3d30972e
Abhijeet Pardeshi <abhijeet.pardeshi1997gmail@.com>
2021-07-30 09:50:08 +0530

    stateful

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import './App.css'
 import { useState } from 'react'
 
 function App() {
-  let [name] = useState('Abhijeet')
-  let [counter] = useState(100)
-  let [active] = useState(true)
-  let [list] = useState(['Kunal', 'akash', 'tushar'])
-  let [std1] = useState({ name: 'aditya' })
+  let [name, setName] = useState('Abhijeet')
+  let [counter, setCounter] = useState(100)
+  let [active, setActive] = useState(true)
+  let [list, setList] = useState(['Kunal', 'akash', 'tushar'])
 
   return (
     <div>
-      <h1>learning to declare stateful variables</h1>
-      <h1>String :: {name}</h1>
-      <h1>Number :: {counter}</h1>
-      <h1>Boolean :: {active + ''}</h1>
-      <h1>List ::{list}</h1>
-      <h1>Object ::{std1.name}</h1>
-      <h1>Object :: {JSON.stringify(std1)}</h1>
+      <h1>Stateful continue</h1>
+      <h1>counter :: {counter}</h1>
+      <h1>List :: {list}</h1>
     </div>
   )
 }
