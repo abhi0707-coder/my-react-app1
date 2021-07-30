3035d6c82aade2cc67c43410b805d17c7549e03f
Abhijeet Pardeshi <abhijeet.pardeshi1997gmail@.com>
2021-07-31 00:18:01 +0530

    make two folder and import into ne

--- src/App.js
+++ src/App.js
@@ -1,47 +1,18 @@
 import logo from './logo.svg'
 import './App.css'
 import { useState } from 'react'
+import { HelloWorld } from './pages/HelloWorld'
+import { HelloUniverse } from './pages/HelloUniverse'
 
 export default function App() {
   return (
     <div>
       <HelloWorld />
       <HelloWorld />
+      <HelloWorld />
 
       <HelloUniverse />
       <HelloUniverse />
-    </div>
-  )
-}
-
-// TAGS :: COMPONENTS :: Follow PascalCase Convention
-// <HelloWorld />
-function HelloWorld() {
-  return (
-    <div className="bg-dark text-danger p-3 mb-1 ">
-      <h1>Hello World</h1>
-      <hr />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque adipisci
-        mollitia saepe perferendis rem labore est, itaque quis error, explicabo,
-        autem fugiat quo dolorem beatae? Nihil dolor sit labore officia?
-      </p>
-    </div>
-  )
-}
-
-// <HelloUniverse />
-function HelloUniverse() {
-  return (
-    <div className="bg-success text-light p-3 my-1">
-      <h1>Hello Universe</h1>
-      <hr />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. In excepturi
-        quidem ratione aut odio libero alias quasi, nisi, temporibus vero
-        assumenda. Quia, dicta. Voluptates rerum voluptatum ducimus itaque sequi
-        dicta?
-      </p>
     </div>
   )
 }
