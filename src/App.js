773eb1ad45ce21d874b4bcd2a35db28c2c281501
Abhijeet Pardeshi <abhijeet.pardeshi1997gmail@.com>
2021-07-31 01:58:18 +0530

    demo css heading desc

--- src/App.js
+++ src/App.js
@@ -1,73 +1,28 @@
 import logo from './logo.svg'
 import './App.css'
-import { useState } from 'react'
-import { HelloWorld } from './pages/HelloWorld'
-import { HelloUniverse } from './pages/HelloUniverse'
-import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 export default function App() {
   return (
     <div>
-      <MyComponent />
+      <MyComponent heading="Java" desc="Java is cool." />
+      <MyComponent heading="Javascript" desc="Javascript is Hot!!" />
+      <MyComponent heading="Python" desc="Python is Intelligent!!" />
     </div>
   )
 }
 
-function MyComponent() {
-  let [list, setList] = useState([])
-
-  //1
-  let [thought, setThought] = useState('')
-
-  let postYourThought = () => {
-    let newlist = [thought, ...list]
-    setList(newlist)
-    // clear the input:thougth
-    setThought('')
-  }
-  //4
-  let handleInputChange = (e) => {
-    console.log(e.target.value)
-    let newthought = e.target.value
-    setThought(newthought)
-  }
-  // ENTER-2
-  const handleKeyEvent = (e) => {
-    // console.log(e.key, e.keyCode);
-    if (e.keyCode == 13) {
-      // console.log("User Has Pressed Entered....");
-      postYourThought()
-    }
-  }
-
+// Reading the PROPS passed by the Parent.
+function MyComponent({ heading, desc }) {
   return (
-    <div className="m-2">
-      <h1 className="bg-primary rounded p-3 sticky-top">
-        Work With Minifacebook
-      </h1>
-
-      <input
-        type="text"
-        className="form-control form-control-lg my-2"
-        style={{ height: 70 }}
-        placeholder="Post Your Thoughts"
-        value={thought} //2
-        onChange={handleInputChange} //3
-        onKeyDown={handleKeyEvent} // ENTER-1
-      />
-      <input
-        type="
-          button"
-        className="btn btn-dark w-100"
-        value="POST THOUGHTS"
-        onClick={postYourThought}
-      />
-      <div className="h1 border p-2 bg-primary my-1 text-danger ">
-        Thoughts List
-      </div>
-      {list.map((item) => {
-        return <div className="alert alert-warning mt-2">{item}</div>
-      })}
+    <div className="bg-dark p-3 text-light mb-1">
+      <h1>Learning {heading}</h1>
+      <hr />
+      <p>
+        <mark className="rounded alert-warning">{desc}</mark> Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Earum harum iusto, repellendus
+        officia cumque officiis amet sed aspernatur rem assumenda recusandae
+        odio ipsa libero a voluptas voluptate, facere reiciendis aliquam?
+      </p>
     </div>
   )
 }
