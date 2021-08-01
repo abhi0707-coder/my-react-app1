cc47733de4e8f17635255e6b4af9bd4ac862440f
Abhijeet Pardeshi <abhijeet.pardeshi1997gmail@.com>
2021-08-01 23:02:44 +0530

    coplete form with insert

--- src/App.js
+++ src/App.js
@@ -1,44 +1,125 @@
-import logo from './logo.svg'
-import './App.css'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 export default function App() {
   return (
     <div>
       <MyRegisterComponent />
     </div>
-  )
+  );
 }
 function MyRegisterComponent() {
-  const [list, setList] = useState([]);
+  let [userList, setUserList] = useState([
+    { id: 1, name: "rahul", email: "rahul@gmail.com", mobile: "212121" },
+  ]);
 
-  // React.
-  // useEffect();
-  // useEffect(() => {}, []);
-  useEffect(() => {
-    console.log("I am special. like your constructor.");
-    makeAjaxBackendApiCall();
-  }, []);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
 
+  const usernameChangeHandler = (e) => setUsername(e.target.value);
+  const passwordChangeHandler = (e) => setPassword(e.target.value);
+  const emailChangeHandler = (e) => setEmail(e.target.value);
+  const mobileChangeHandler = (e) => setMobile(e.target.value);
 
+  const addNewUser = () => {
+    const newuser = {
+      id: userList.length + 1,
+      name: username,
+      password: password,
+      email: email,
+      mobile: mobile,
+    };
+    const newUserList = [newuser, ...userList];
+    setUserList(newUserList);
 
-  const makeAjaxBackendApiCall = async () => {
-    const url = "https://jsonplaceholder.typicode.com/posts";
-    const result = await axios.get(url)
-    //console.log(result.data)
-
-    setList(result.data);
+    // After Success
+    setUsername("");
+    setPassword("");
+    setEmail("");
+    setMobile("");
   };
   return (
     <div>
-      <h1 className="bg-primary p-3 text-danger">Lets connect with backend</h1>
+      <h1 className="bg-dark text-light p-3 ">User Registeation </h1>
 
+      {/** FORM COMPONENT */}
+      <form className="m-2">
+        <div>
+          <input
+            type="text"
+            className="form-control form-control-lg mb-1"
+            placeholder="Enter username"
+            value={username}
+            onChange={usernameChangeHandler}
+          />
+        </div>
 
-      {list.map((item) => {
-        return <div className="alert  alert-primary text-capitalize">
-          {item.title}</div>;
-      })}
+        <div>
+          <input
+            type="password"
+            className="form-control form-control-lg mb-1"
+            placeholder="Enter Passwword"
+            value={password}
+            onChange={passwordChangeHandler}
+          />
+        </div>
+
+        <div>
+          <input
+            type="email"
+            className="form-control form-control-lg mb-1"
+            placeholder="Enter Email"
+            value={email}
+            onChange={emailChangeHandler}
+          />
+        </div>
+
+        <div>
+          <input
+            type="mobile"
+            className="form-control form-control-lg mb-1"
+            placeholder="Enter Mobile"
+            value={mobile}
+            onChange={mobileChangeHandler}
+          />
+        </div>
+
+        <div>
+          <input
+            type="button"
+            value="Register"
+            onClick={addNewUser}
+            className="btn btn-lg btn-secondary w-100"
+          />
+        </div>
+      </form>
+      {/** List BOX HERE */}
+      <table className="table table-dark table-striped m-2">
+        <thead>
+          <tr>
+            <th scope="col">#ID</th>
+            <th scope="col">USERNAME</th>
+            <th scope="col">PASSWORD</th>
+            <th scope="col">EMAIL</th>
+            <th scope="col">MOBILE</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userList.map((item) => {
+            return (
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>#####</td>
+                <td>{item.email}</td>
+                <td>{item.mobile}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
